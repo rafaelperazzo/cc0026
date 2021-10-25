e4df4ba87a8cc3a7a8771c40702d330a12023b89
Rafael Perazzo <rafaelperazzo@gmail.com>
2021-10-25 14:59:37 -0300

    Exemplo 3 - tarefa

---
+++ lab1/exemplo3_alterado.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+int main(void) {
+   pid_t pid = fork();
+   if (pid<0) {
+       printf("Falha ao criar o processo!\n");
+   }
+   else if (pid==0) { //Processo filho
+        printf("[FILHO]: PID %d - PPID: %d\n",getpid(),getppid());
+        printf("Vou dormir um pouco...\n");
+        sleep(60);
+   }
+   else { //Processo pai
+        int status;
+        printf("[PAI]: PID %d - PPID: %d\n",getpid(),getppid());
+        wait(&status);
+        if (WIFSIGNALED(status)) {
+            printf("[PAI]: Filho (%d) terminou devido a um sinal: %d\n",pid,WTERMSIG(status));
+        }
+        else {
+            printf("[PAI]: Filho (%d) nao terminou devido a um sinal\n");
+        }
+   }
+   return 0;
+}
+
